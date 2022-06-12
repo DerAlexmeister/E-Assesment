99e735781fdc26d32a7573695cabe91ac8a2dce4
dokunogakusha <94387179+dokunogakusha@users.noreply.github.com>
2022-06-12 19:47:49 +0200

    Implement model for cloze

--- EAssLAna/EAss/models.py
+++ EAssLAna/EAss/models.py
@@ -1,7 +1,3 @@
-from dataclasses import dataclass
-
-from typing import List, Set
-
 from django.db import models
 
 
@@ -45,46 +41,17 @@ class BinaryStatement(models.Model):
         return "Random expression: {}".format(self.Length)
 
 
-class GapModel(models.Model):
+class Gap(models.Model):
     preceeding_text = models.TextField(blank=False)
-    suceeding_text = models.TextField(blank=False)
+    succeeding_text = models.TextField(blank=False)
 
 
 class GapSolution(models.Model):
     solution = models.TextField(blank=False, null=False)
-    gap = models.ForeignKey(GapModel, on_delete=models.CASCADE)
+    gap = models.ForeignKey(Gap, on_delete=models.CASCADE)
 
 
-class ClozeModel(models.Model):
+class Cloze(models.Model):
+    qaw = models.ForeignKey(QAWSet, on_delete=models.CASCADE)
+    gap = models.ForeignKey(Gap, on_delete=models.CASCADE)
     position = models.PositiveIntegerField()
-    gap = models.ForeignKey(GapSolution, on_delete=models.CASCADE)
-
-
-@dataclass
-class Gap:
-    preceeding_text: str
-    succeeding_text: str
-    solutions: Set[str]
-
-
-@dataclass
-class Cloze:
-    gaps: List[Gap]
-
-
-def create_cloze_model(cloze: Cloze):
-    for i, gap in enumerate(cloze.gaps):
-        gap_model = GapModel(
-            preceeding_text=gap.preceeding_text,
-            succeeding_text=gap.succeeding_text,
-        )
-        gap_model.save()
-
-        ClozeModel(position=i, gap=gap).save()
-
-        for solution in gap.solutions:
-            solution_model = GapSolution(
-                solution=solution,
-                gap=gap_model,
-            )
-            solution_model.save()
