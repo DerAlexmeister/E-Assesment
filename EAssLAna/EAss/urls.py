23f07a06a194bba23e95b079a151302ec81003b4
DerAlexmeister <alexweb199@hotmail.de>
2022-06-16 18:02:37 +0200

    (Added) start page

--- EAssLAna/EAss/urls.py
+++ EAssLAna/EAss/urls.py
@@ -1,11 +1,15 @@
+from textwrap import indent
 from django.contrib import admin
 from django.urls import path
 
-from .views import generateExample
-from .views import generateBinaryExpression, clozeText
+from .views import index
+from .views import generateMCExample
+from .views import generateBinaryExpression
+from .views import clozeText
 
 urlpatterns = [
-    path('example/', generateExample),
+    path('', index),
+    path('example/', generateMCExample),
     path('examplebin/', generateBinaryExpression),
-    path('cloze/', clozeText)
+    path('examplecloze/', clozeText),
 ]
